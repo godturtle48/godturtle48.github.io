d6ee99ca1f09b8455c3b1caf312a1f1519b69601
godturtle48 <beobeo1998@gmail.com>
2019-02-05 23:54:21 +0700

    add scroll function

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,32 @@
-$(document).ready(function(){
+$(function(){
     $('#btnStart').click(function () {
-        $('html').scrollTo('#information-section');
+        $("html,body").animate({scrollTop:$('#information-section').offset().top});
+        return false;
     })
-    
+    $('#btnInfo').click(function () {
+        $("html,body").animate({scrollTop:$('#information-section').offset().top});
+        return false;
+    })
+    $('#btnEducation').click(function () {
+        $("html,body").animate({scrollTop:$('#education-section').offset().top});
+        return false;
+    })
+    $('#btnExperience').click(function () {
+        $("html,body").animate({scrollTop:$('#education-section').offset().top});
+        return false;
+    })
+    $('#btnTop').click(function () {
+        $("html,body").animate({scrollTop:0});
+        return false;
+    })
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 50) {
+            $('.navbar').addClass("white-bg");
+            $('.nav-item').addClass("black-text");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $(".navbar").removeClass("white-bg");
+            $(".nav-item").removeClass("black-text");
+        }
+    })
 })
